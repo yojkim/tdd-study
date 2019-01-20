8d2852d4e8771b4b34573410c79a7d4f2df06983
Yong Jae Kim <youthful2016@gmail.com>
2019-01-20 17:58:02 +0900

    #14 ch22 done

---
+++ ch22/yojkim/ch22/test_testcase.py
@@ -0,0 +1,32 @@
+from testcase import TestCase
+from wasrun import WasRun
+from testresult import TestResult
+
+
+class TestCaseTest(TestCase):
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+
+
+TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResultFormatting").run()
+TestCaseTest("testFailedResult").run()
