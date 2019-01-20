75ac8b16ced7b6a8fe7aeedbcad5eab6b3af683a
Yong Jae Kim <youthful2016@gmail.com>
2019-01-20 15:29:25 +0900

    #14 ch18 done

---
+++ ch18/yojkim/ch18/test_testcase.py
@@ -0,0 +1,13 @@
+from testcase import TestCase
+from wasrun import WasRun
+
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+
+TestCaseTest("testRunning").run()
